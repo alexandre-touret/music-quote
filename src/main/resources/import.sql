c737bc472bbf6ee4879b6d7a5f23879fbdb46fc6
Alexandre Touret <alexandre.touret@free.fr>
2020-05-02 22:34:40 +0200

    Add some content ( thanks miles davis ) Add K8S load balancer to enable querying api

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,18 @@
 insert into MusicQuote (id, author, quote)
-values (1, 'author', 'quote');
+values (1, 'Miles Davis','If you understood everything I said, you\’d be me');
 insert into MusicQuote (id, author, quote)
-values (2, 'author', 'quote');
+values (2, 'Miles Davis', 'Man, sometimes it takes you a long time to sound like yourself.');
 insert into MusicQuote (id, author, quote)
-values (3, 'author', 'quote');
+values (3, 'Miles Davis', 'Don''t play what''s there; play what''s not there.');
+insert into MusicQuote (id, author, quote)
+values (4, 'Miles Davis', 'Do not fear mistakes - there are none. ');
+insert into MusicQuote (id, author, quote)
+values (5, 'Miles Davis', 'It’s like, how did Columbus discover America when the Indians were already here? What kind of shit is that, but white people’s shit?');
+insert into MusicQuote (id, author, quote)
+values (6, 'Miles Davis', 'My future starts when I wake up every morning. Every day I find something creative to do with my life.');
+insert into MusicQuote (id, author, quote)
+values (7, 'Miles Davis', 'Knowledge is freedom and ignorance is slavery');
+insert into MusicQuote (id, author, quote)
+values (8, 'Miles Davis', 'Anybody can play. The note is only 20 percent. The attitude of the motherfucker who plays it is 80 percent.');
+insert into MusicQuote (id, author, quote)
+values (9, 'Miles Davis', 'When you’re creating your own shit, man, even the sky ain’t the limit..');
